f506d407522dc1266ac77d2f7fa01094939feaac
MartyInFuture <martyinfuture@gmail.com>
2021-08-07 13:27:21 +0300

    add path of functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import './sass/main.scss';
-import config from './config.json';
-import { getDataServer, renderContent } from './js/functions';
+import { refs } from './js/refs';
+import { renderContent } from './js/functions';
 
 const getPath = () => {
-  const allPath = location.pathname + location.search;
-  return { allPath, pathname: location.pathname, search: location.search };
+  return location.pathname + location.search;
 };
 
 renderContent(getPath());
+document.addEventListener('click', e => {
+  if (e.target.closest('a')) {
+    e.preventDefault();
+    const path = e.target.getAttribute('href');
+    history.pushState(null, null, path);
+    renderContent(path);
+  } else if (e.target.closest('button')) {
+    e.preventDefault();
+    const input = refs.header.querySelector('.search__input');
+    if (input.value != '') {
+      const path = input.dataset.search + input.value;
+      renderContent(path);
+    }
+  } else {
+    return false;
+  }
+});
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  ads: document.querySelector('#ads'),
+  header: document.querySelector('#header'),
+  footer: document.querySelector('#footer'),
+  content: document.querySelector('#content'),
+};
